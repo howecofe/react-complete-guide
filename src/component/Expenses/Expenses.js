1b22e48c02874d32f6842137c1188a1df340a9c5
howecofe <am0229@naver.com>
2023-03-15 00:49:30 +0900

    3. 리액트 State 및 이벤트: 이벤트 및 State 연습

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,12 +1,22 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpensesFilter';
 import './Expenses.css';
 
 // arrow 함수
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
 
     return (
+        <div>
         <Card className="expenses">
+            <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
@@ -28,6 +38,7 @@ const Expenses = (props) => {
                 date={props.items[3].date}
             />
         </Card>
+        </div>
     );
 }
 
