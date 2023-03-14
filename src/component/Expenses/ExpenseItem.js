dd189e67bc9c649d80e269574e59368142cc3d5a
howecofe <am0229@naver.com>
2023-03-15 01:03:59 +0900

    3. 리액트 State 및 이벤트: ExpenseItem.js 버튼 및 State 제거

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -11,21 +11,21 @@ const ExpenseItem = (props) => {
     // 오직 컴포넌트 함수 내에서 호출되어야 하고, 길이 2의 배열을 반환
     // 배열의 첫 번째 요소(title) : 관리되고 있는 값, 현재 상태값
     // 배열의 두 번째 요소(setTitle) : 나중에 새로운 title을 설정하기 위해 호출될 수 있는 함수 (첫 번째 요소의 값을 업데이트하는 함수)
-    const [title, setTitle] = useState(props.title);
+    // const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    };
+    // const clickHandler = () => {
+    //     setTitle('Updated!');
+    //     console.log(title);
+    // };
 
     return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
     );
 }
