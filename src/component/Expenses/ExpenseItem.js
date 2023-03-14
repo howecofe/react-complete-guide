23ab3c3346d288e30e1f0bd3fe7c56f1d75a5516
howecofe <am0229@naver.com>
2023-03-14 21:22:59 +0900

    3. 리액트 State 및 이벤트: useState 훅

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
@@ -5,10 +7,14 @@ import './ExpenseItem.css';
 // arrow 함수
 const ExpenseItem = (props) => {
 
-    let title = props.title;
+    // 리액트 훅 : useState
+    // 오직 컴포넌트 함수 내에서 호출되어야 하고, 길이 2의 배열을 반환
+    // 배열의 첫 번째 요소(title) : 관리되고 있는 값, 현재 상태값
+    // 배열의 두 번째 요소(setTitle) : 나중에 새로운 title을 설정하기 위해 호출될 수 있는 함수 (첫 번째 요소의 값을 업데이트하는 함수)
+    const [title, setTitle] = useState(props.title);
 
     const clickHandler = () => {
-        title = 'Updated!'
+        setTitle('Updated!');
         console.log(title);
     };
 
