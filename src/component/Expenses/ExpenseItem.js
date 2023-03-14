8cbb18ad7e7c6326f8f142432447e4882da4ebce
howecofe <am0229@naver.com>
2023-03-15 04:03:37 +0900

    4. 리스트 렌더링 및 조건부 Content: 조건부 Content 출력하기 2

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,33 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 // arrow 함수
 const ExpenseItem = (props) => {
+  // 리액트 훅 : useState
+  // 오직 컴포넌트 함수 내에서 호출되어야 하고, 길이 2의 배열을 반환
+  // 배열의 첫 번째 요소(title) : 관리되고 있는 값, 현재 상태값
+  // 배열의 두 번째 요소(setTitle) : 나중에 새로운 title을 설정하기 위해 호출될 수 있는 함수 (첫 번째 요소의 값을 업데이트하는 함수)
+  // const [title, setTitle] = useState(props.title);
 
-    // 리액트 훅 : useState
-    // 오직 컴포넌트 함수 내에서 호출되어야 하고, 길이 2의 배열을 반환
-    // 배열의 첫 번째 요소(title) : 관리되고 있는 값, 현재 상태값
-    // 배열의 두 번째 요소(setTitle) : 나중에 새로운 title을 설정하기 위해 호출될 수 있는 함수 (첫 번째 요소의 값을 업데이트하는 함수)
-    // const [title, setTitle] = useState(props.title);
+  // const clickHandler = () => {
+  //     setTitle('Updated!');
+  //     console.log(title);
+  // };
 
-    // const clickHandler = () => {
-    //     setTitle('Updated!');
-    //     console.log(title);
-    // };
-
-    return (
-    <Card className="expense-item">
-        <ExpenseDate date={props.date}/>
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
         </div>
         {/* <button onClick={clickHandler}>Change Title</button> */}
-    </Card>
-    );
-}
+      </Card>
+    </li>
+  );
+};
 
 export default ExpenseItem;
--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpensesFilter";
 import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
 
 // arrow 함수
 const Expenses = (props) => {
@@ -17,20 +17,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  // 변수에 JSX 코드 할당도 가능
-  let expensesContent =  <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -38,7 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
