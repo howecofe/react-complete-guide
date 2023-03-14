77a0aa3323c7ca54e0dbe67fb33be61c6e975d7a
howecofe <am0229@naver.com>
2023-03-15 00:07:00 +0900

    3. 리액트 State 및 이벤트: 자식에서 부모 컴포넌트로 데이터 전달하기(props)

--- src/App.js
+++ src/App.js
@@ -33,10 +33,15 @@ const App = () => {
   //   React.createElement(Expenses, { items: expenses})
   // );
 
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
 
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/component/NewExpense/NewExpense.js
+++ src/component/NewExpense/NewExpense.js
@@ -3,14 +3,21 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData); // 부모 컴포넌트의 포인터 함수 호출
+
+    };
 
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
         </div>
     );
-
 }
 
 export default NewExpense;
