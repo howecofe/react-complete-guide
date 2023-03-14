d8e8dab6075d66c259a4e314499935409cf94a73
howecofe <am0229@naver.com>
2023-03-14 22:48:30 +0900

    3. 리액트 State 및 이벤트: State 방법 3가지

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -5,17 +5,58 @@ const ExpenseForm = () => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // });
 
     const titleChangeHandler = (event) => {
+        // 방법 1
         setEnteredTitle(event.target.value);
+
+        // 방법 2
+        // setUserInput({
+        //     ...userInput, // ...userInput : 기존에 userInput에 있던 값(키:값)을 새로운 객체로 복사 (이전 상태 의존)
+        //     enteredTitle: event.target.value // enteredTitle는 Overriding
+        // });
+
+        // 방법 3 - prevState: 이전 상태 의존 시 사용
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value };
+        // })
     };
 
     const amountChangeHandler = (event) => {
+        // 방법 1
         setEnteredAmount(event.target.value);
+
+        // 방법 2
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value
+        // });
+
+        // 방법 3
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredAmount: event.target.value };
+        // })
     }
 
     const dateChangeHandler = (event) => {
+        // 방법 1
         setEnteredDate(event.target.value);
+
+        // 방법 2
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value
+        // });
+
+        // 방법 3
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredDate: event.target.value };
+        // })
     }
 
     return (
